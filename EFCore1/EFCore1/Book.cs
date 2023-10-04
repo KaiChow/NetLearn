aba220bfcdd813cb33828b7d3cd1792a1e1dbd18
KaiChow <zhoukai37927@163.com>
2023-10-04 23:13:19 +0800

    使用efcore

---
+++ EFCore1/EFCore1/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace EFCore1
+{
+    /// <summary>
+    /// 1,Add-Migration XXXX
+    /// 2,Update-Database
+    /// </summary>
+    public class Book
+    {
+        public long Id { get; set; }
+        public string? Title { get; set; }
+        public DateTime PubTime { get; set; }
+        public double Price { get; set; }
+
+    }
+}
---
+++ EFCore1/EFCore1/BookConfig.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EFCore1
+{
+    public class BookConfig : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.ToTable("T_Books");
+            builder.Property<string>("Title").HasMaxLength(50).IsRequired();
+        }
+    }
+}
