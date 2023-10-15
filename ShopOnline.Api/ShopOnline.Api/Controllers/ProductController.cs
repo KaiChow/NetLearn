0bff2078f29b2fa103aa093e7ae60bd2b992270a
KaiChow <zhoukai37927@163.com>
2023-10-15 13:25:21 +0800

    在线购物的项目

---
+++ ShopOnline.Api/ShopOnline.Api/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShopOnline.Api.Extensions;
+using ShopOnline.Api.Repositories.Contracts;
+using ShopOnline.Models.Dtos;
+
+namespace ShopOnline.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetList()
+        {
+            try
+            {
+                var products = await productRepository.GetProducts();
+                var productCategories = await productRepository.GetCategories();
+                if (products == null || productCategories == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    var productDtos = products.ConvertToDto(productCategories);
+                    return Ok(productDtos);
+                }
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "数据库错误");
+            }
+        }
+    }
+}
---
+++ ShopOnline.Api/ShopOnline.Api/Repositories/Contracts/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using ShopOnline.Api.Entities;
+
+namespace ShopOnline.Api.Repositories.Contracts
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetItem(int id);
+        Task<IEnumerable<ProductCategory>> GetCategories();
+        Task<ProductCategory> GetCategory(int id);
+    }
+}
